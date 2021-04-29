3f1dde8e62987724336b976b54e62696cde3e2b2
ripinsurreality <ripinsurreality@gmail.com>
2021-04-29 16:18:01 +0300

    Add images to details and landing, fix datepicker props

--- src/modules/datepicker/datepicker.ts
+++ src/modules/datepicker/datepicker.ts
@@ -1,11 +1,18 @@
 import { Model } from "./Model"
 import { View } from "./View"
 import { Controller } from "./Controller"
+import "./_datepicker.sass"
+
 ;(function ($) {
-	$.fn.datepicker = function ({ date, multi }: DatepickerProps) {
+	$.fn.datepicker = function (props?: DatepickerProps) {
 		const model = new Model()
 		const view = new View()
-		const controller = new Controller(model, view, date, multi)
+		const controller = new Controller(
+			model,
+			view,
+			props?.date,
+			props?.multi
+		)
 
 		this.append(controller.render)
 		return this
--- src/pages/landing/index.ts
+++ src/pages/landing/index.ts
@@ -1,8 +1,11 @@
 import "./index.sass"
 import "@modules/dropdown/dropdown"
+import "@modules/datepicker/datepicker"
 
 ;(function ($, window, document) {
 	$(function () {
 		$(".finder__dropdown").dropdown("guests")
+		$(".finder__arrival-datepicker").datepicker()
+		$(".finder__departure-datepicker").datepicker()
 	})
 })(jQuery, window, document)
