3dfe65e85b0c928f76c104d50c484754ad00adb5
ripinsurreality <ripinsurreality@gmail.com>
2021-05-01 20:09:08 +0300

    Search page: add datepicker, dropdowns; add images and info to cards

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,8 @@
 // import "./index.sass"
+import "@modules/datepicker/datepicker"
 
 ;(function ($, window, document) {
 	$(function () {
+		$(".dtpckr").datepicker({multi: true})
 	})
 })(jQuery, window, document)
