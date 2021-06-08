2ffba508a36e820e907cfdda7925b09a9b10aafe
ripinsurreality <ripinsurreality@gmail.com>
2021-06-08 15:05:42 +0300

    nav: add dropdown lists

--- src/modules/nav/nav.ts
+++ src/modules/nav/nav.ts
@@ -3,15 +3,31 @@
 	const items = $(`.nav__items`)
 	const burger = $(`.nav__burger`)
 	const burgerLines = $(`.nav__burger-line`)
-	burger.on("click", toggle)
-	modal.on("click", toggle)
+	burger.on("click", toggleNav)
+	modal.on("click", toggleNav)
 
-	function toggle() {
+	function toggleNav() {
 		modal.toggleClass(`nav-modal--active`)
 		items.toggleClass(`nav__items--active`)
-		burgerLines.each(function() {
+		burgerLines.each(function () {
 			$(this).toggleClass(`nav__burger-line--active`)
 		})
-		// burger.toggleClass(`nav__burder--active`)
 	}
+
+	const listItems = $(`.nav__item--list`)
+	listItems.each((i, listItem) => {
+		const li = $(listItem)
+		const list = li.find(`.nav__list`)
+		const onLiClick = (e: JQuery.ClickEvent) => {
+			list.toggleClass(`nav__list--open`)
+		}
+		const onDocumentClick = (e: JQuery.ClickEvent) => {
+			if (!(e.target instanceof HTMLElement)) {return}
+			if (li.has(e.target).length > 0) {return}
+			const open = list.hasClass(`nav__list--open`)
+			open && list.removeClass(`nav__list--open`)
+		}
+		li.on("click", onLiClick)
+		$(document).on("click", onDocumentClick)
+	})
 })(jQuery)
