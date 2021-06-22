01b1426d18068b4fcc411b7dea890147afbc2c9e
ripinsurreality <ripinsurreality@gmail.com>
2021-06-22 11:40:25 +0300

    landing page: import finder

--- src/pages/landing/index.ts
+++ src/pages/landing/index.ts
@@ -1,12 +1,8 @@
 import "./index.sass"
-import "@modules/dropdown/dropdown"
-import "@modules/datepicker/datepicker"
 import "@modules/nav/nav"
+import "@modules/finder/finder"
 
 ;(function ($, window, document) {
 	$(function () {
-		$(".finder__dropdown").dropdown("guests")
-		$(".finder__arrival-datepicker").datepicker()
-		$(".finder__departure-datepicker").datepicker()
 	})
 })(jQuery, window, document)
